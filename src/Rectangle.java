ade06a02edb1551d0ef7052ac17877552c58ef82
Mawadda <moodalshukaili99@gmail.com>
2026-05-08 16:20:48 +0400

    Creating a new class called Rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle {
+    private double Length;
+    private double Width;
+
+    private static void main(String[] args){
+        Rectangle R1 = new Rectangle(); //Creating a new object from the rectangle class
+
+    }
+}
