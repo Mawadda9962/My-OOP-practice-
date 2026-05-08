d4a0090e7ccdc7d47dc271b8df55735cac979932
Mawadda <moodalshukaili99@gmail.com>
2026-05-08 17:09:30 +0400

    Adding a new object

--- src/CellPhone.java
+++ src/CellPhone.java
@@ -3,8 +3,36 @@ public class CellPhone {
     private  String modle;
     private  double retailPrice;
 
+    public String getManufact() {
+        return manufact;
+    }
+
+    public void setManufact(String manufact) {
+        this.manufact = manufact;
+    }
+
+    public String getModle() {
+        return modle;
+    }
+
+    public void setModle(String modle) {
+        this.modle = modle;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+
+    public static void main(String[] args){
+
+        CellPhone Cell1 = new CellPhone();
 
 
 
+    }
 
 }
