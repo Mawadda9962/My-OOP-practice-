f1304a876fa4b03beb4d6cb5e9cfc3a5539a0794
Mawadda <moodalshukaili99@gmail.com>
2026-05-08 16:44:22 +0400

    Adding a new method called area

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,8 +2,41 @@ public class Rectangle {
     private double Length;
     private double Width;
 
+    public double getLength() {
+        return Length;
+    }
+
+    public void setLength(double length) {
+        Length = length;
+    }
+
+    public double getWidth() {
+        return Width;
+    }
+
+    public void setWidth(double width) {
+        Width = width;
+    }
+
+    public double getarea(){
+       return Length * Width;
+    }
+
     private static void main(String[] args){
         Rectangle R1 = new Rectangle(); //Creating a new object from the rectangle class
-        ..
+
+        R1.setLength(2);
+        R1.setWidth(4);
+
+        System.out.println(R1.Length);
+
+
+
+        Rectangle R2 = new Rectangle();
+
+        R2.setWidth(4);
+        R2.setLength(4);
+
+
     }
 }
